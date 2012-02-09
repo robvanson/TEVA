e8a9fe49194bb34b1add1f2efdcdfedde603892e
rob <rob@MacJames.local>
2012-02-09 10:44:26 +0100

    Reorganizing ManPages to include language versions

--- HTML/ConvertHTMLpages.sh
+++ HTML/ConvertHTMLpages.sh
@@ -19,12 +19,12 @@ for file in $(ls -1 ${@:-*.html}); do
 			${file}
 	fi
 	
-	if [[ "NKI_TE-VOICE_ANALYSIS_tool.html Recording_your_own_voice.html Opening_an_existing_recording.html Adding_speaker_information.html Selecting_stable_sounds.html Saving_a_report.html " =~ ${file} ]]; then
+	if [[ "NKI_TE-VOICE_ANALYSIS_tool__EN_.html Recording_your_own_voice.html Opening_an_existing_recording.html Adding_speaker_information.html Selecting_stable_sounds.html Saving_a_report.html " =~ ${file} ]]; then
 		perl -i'' -ne 's!(\Q</b></font></table></table>\E)\s*$!\1<p align=center>\n<a href="Main_page_screenshot.png" target="_blank"><img width="33%" src="Main_page_screenshot.png" VSPACE=10></a>\n</p>\n!g;print $_' \
 			${file}
 	fi
 	
-	if [[ "${file}" == "NKI_TE-VOICE_ANALYSIS_tool.html" ]]; then
+	if [[ "${file}" == "NKI_TE-VOICE_ANALYSIS_tool__EN_.html" ]]; then
 		perl -i'' -ne 's!(\Q<img width="33%" src="Main_page_screenshot.png" VSPACE=10></a>\E)\s*$!\1&nbsp;<a href="Configure_page_screenshot.png" target="_blank"><img width="33%" src="Configure_page_screenshot.png" VSPACE=10></a>\n!g;print $_' \
 			${file}
 	fi
