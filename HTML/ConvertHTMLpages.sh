e716a879e521649463b1a0a0416445d1ca5bc2da
Rob van Son <r.j.j.h.vanson@gmail.com>
2014-05-15 11:05:57 +0200

    Some HTML stuff

--- HTML/ConvertHTMLpages.sh
+++ HTML/ConvertHTMLpages.sh
@@ -31,13 +31,15 @@ sed -i ""  's/Example_evaluating_a_Corpus_2.png/Main_page_serial_detail_Nxt.png/
 rm -f Example_evaluating_a_Corpus_2.png
 sed -i ""  's/Example_evaluating_a_Corpus_3.png/Configure_page_serial_detail.png/g' Example_evaluating_a_Corpus.html
 rm -f Example_evaluating_a_Corpus_3.png
-sed -i ""  's/NKI_TE-VOICE_ANALYSIS_tool__EN__1.png/Main_page_screenshot.png/g' NKI_TE-VOICE_ANALYSIS_tool__EN_.html
+
+perl -i'' -ne 'if(m!(<a href\=\"NKI_TE-VOICE_ANALYSIS_tool__EN__1.png\" target\=\"_blank\"><img height=\d+ width=\d+ src=NKI_TE-VOICE_ANALYSIS_tool__EN__1.png></a>)!){$a = $1;$b = $a;$a=~s/NKI_TE-VOICE_ANALYSIS_tool__EN__1.png/Main_page_screenshot.png/g;$b=~s/NKI_TE-VOICE_ANALYSIS_tool__EN__1.png/Configure_page_screenshot.png/g;s!(<a href="NKI_TE-VOICE_ANALYSIS_tool__EN__1.png" target="_blank"><img height=250 width=450 src=NKI_TE-VOICE_ANALYSIS_tool__EN__1.png></a>)!$a$b!g;};print $_' NKI_TE-VOICE_ANALYSIS_tool__EN_.html
 rm -f NKI_TE-VOICE_ANALYSIS_tool__EN__1.png
-perl -i'' -ne 's!((<a href=\")(Main_page_screenshot.png)(\"[^>]*><img [^>]+)(Main_page_screenshot.png)([^>]*></a>))!\1&nbsp;\2Configure_page_screenshot.png\4Configure_page_screenshot.png\6!g; print $_' NKI_TE-VOICE_ANALYSIS_tool__EN_.html
 perl -i'' -ne 's! height\=\d+ ! !ig; s! width\=\d+ ! width=45% !ig; print $_' NKI_TE-VOICE_ANALYSIS_tool__EN_.html
 
 sed -i ""  's/Opening_an_existing_recording_1.png/Main_page_screenshot.png/g' Opening_an_existing_recording.html
 rm -f Opening_an_existing_recording_1.png
+sed -i ""  's/Recording_tasks_1.png/Recording_tasks_window.png/g' Recording_tasks.html
+rm -f Recording_tasks_1.png
 sed -i ""  's/Overview_of_Configuration_page_1.png/Configure_page_screenshot.png/g' Overview_of_Configuration_page.html
 rm -f Overview_of_Configuration_page_1.png
 sed -i ""  's/Overview_of_Main_page_1.png/Main_page_screenshot.png/g' Overview_of_Main_page.html
@@ -48,6 +50,8 @@ sed -i ""  's/Saving_a_report_1.png/Main_page_screenshot.png/g' Saving_a_report.
 rm -f Saving_a_report_1.png
 sed -i ""  's/Saving_a_report_2.png/Save_screenshotTop.png/g' Saving_a_report.html
 rm -f Saving_a_report_2.png
+sed -i ""  's/VAS_rating_scales__EN__1.png/VAS_display.png/g' VAS_rating_scales__EN_.html
+rm -f VAS_rating_scales__EN__1.png
 perl -i'' -ne 's!((<a href=\")(Save_screenshotTop.png)(\"[^>]*><img [^>]+)(Save_screenshotTop.png)([^>]*></a>))!\1&nbsp;\2Save_screenshotBottom.png\4Save_screenshotBottom.png\6!g; print $_' Saving_a_report.html
 perl -i'' -ne 's! height\=\d+ ! !ig; s! width\=\d+ ! width=45% !ig; print $_' Saving_a_report.html
 
